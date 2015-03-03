0c18c0f88e0bf3e45cde38345290f6a0c89e3684
Ruddy Stream <ruddystream@gmail.com>
2015-03-02 18:50:09 -0600

    Answer to Chapter 2 Exercise 2 of Learning Perl.

---
+++ chapter-02/ex02-01.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+my $pi      = 3.141592654;
+my $radius  = 12.5;
+
+my $circ    = 2 * $pi * $radius;
+print( "The circumference of a circle of $radius radius is... \n\n\t\t $circ!\n" );
