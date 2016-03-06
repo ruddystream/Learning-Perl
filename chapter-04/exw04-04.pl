67e2f56a62a55c6c0ef7392d1ff9630170cb6d5d
Ruddy Stream <ruddystream@gmail.com>
2016-03-05 23:24:04 -0600

    Answer for Ch. 4, Ex. 4 of Learning Perl Student Workbook.
    Yeah, super simple.
    
    /rs.

---
+++ chapter-04/exw04-04.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+sub show_args {
+	print( "The arguments are @_. \n" );
+}
+
+sub show_args_again {
+	print( "Again, the arguments are: @_. \n" );
+	&show_args;
+	&show_args();
+	show_args(reverse @_);
+}
+
+show_args_again( 'fred', 'barny', 'wilma', 'betty' );
