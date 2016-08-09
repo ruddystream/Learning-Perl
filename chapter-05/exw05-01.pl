1b87599ac1917f13f9ba617bc2b107515af15af6
Ruddy Stream <ruddystream@gmail.com>
2016-08-08 22:22:52 -0600

    Answer for Ch. 5, Ex 1 of Learning Perl Student Workbook
    A program to read lines from all of the files on the command line
    and print out each line prefaced with its filename.
    
    /rs.

---
+++ chapter-05/exw05-01.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+###
+# Chapter 05 Workbook Exercise 01
+# Write a program to read lines from all of the files on the 
+# command line and print out each line prefaced with its filename.
+#
+# usage:
+#   exw05-01.pl input1 input2 input3
+#
+# output:
+#   input1:
+#     asdf
+#   input2:
+#     cxzv
+#   input3:
+#     qwer
+#
+
+use strict;
+use warnings;
+
+my $file;
+
+foreach $file ( @ARGV ) {
+
+	if ( ! open FILETOOUTPUT, "$file" ) {
+		print "couldn't open $file!"
+	} else {
+		print "$file:\n";
+		while ( <FILETOOUTPUT> ) {
+			chomp;
+			print "  $_\n";
+		}
+
+	}
+}
