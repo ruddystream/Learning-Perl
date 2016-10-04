efa2badcd3b583e2b4646bc725f29818e8bcc065
Ruddy Stream <ruddystream@gmail.com>
2016-10-03 18:34:39 -0600

    exw06-03 is also exw06-04.
    Combined 3 & 4 together, since I seem to have skipped over 3, but still
    did it anyhow.
    
    /rs.

--- chapter-06/exw06-03.pl
+++ chapter-06/exw06-03.pl
@@ -1,12 +1,24 @@
 #!/usr/bin/perl
 
 ###
-# Chapter 06 Workbook Exercise 02
+# Chapter 06 Workbook Exercise 03/4
 # Modify the program from Exercise 6.1 so it prompts for the user for a name, 
 # and then reports the birthday for that name.
 #
+# AND
+# 
+# What happens in your program for Exercise 6.3 if the user-entered 
+# name is in the hash, but does not have a birthday? Add a couple of 
+# entries to the hash: one with a value of undef, and one with a value 
+# of the empty string. Modify your program for Exercise 6.3 to tell 
+# the user that you don’t know the birthday if the key is in the hash, 
+# but does not have a value.
+#
+# (Combined 3 & 4 together, since I seem to have skipped over 3, but 
+# still did it anyhow.)
+#
 # usage:
-#   exw06-02.pl 
+#   exw06-04.pl 
 #
 # output:
 #   enter a person's name: 
