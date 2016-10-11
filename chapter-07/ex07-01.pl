f3fcf017e48eab8fb017a5e089c8528968a8b2db
Ruddy Stream <ruddystream@gmail.com>
2016-10-10 20:24:07 -0600

    Answer for Ch. 7, Ex 1 of Learning Perl
    Make a program that prints each line of its input that mentions fred.
    (It shouldn’t do anything for other lines of input.) Does it match if
    your input string is Fred, frederick, or Alfred? Make a small text file
    with a few lines mentioning “fred flintstone” and his friends, then use
    that file as input to this program and the ones later in this section.”
    
    Pretty simple, just read in a file, go through it line by line, and test
    if the line matches the regex /fred/. My answer was more complex than
    the book's, with error checking, and explicitly opening the file.
    
    I also added a test data file, 'testdata'.
    
    /rs.

---
+++ chapter-07/ex07-01.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+###
+# Chapter 07 Exercise 01
+# Make a program that prints each line of its input that mentions fred.
+#
+# usage:
+#   exw07-01.pl
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while ( <DATAFILE> ) {
+		if ( /fred/ ) {
+			print "this line matched: $_";
+		}
+	}
+}
