5bdeb9f9a3cb38a9432eb9956ff29aed6f5c853d
Ruddy Stream <ruddystream@gmail.com>
2016-10-13 14:17:29 -0600

    Answer for Ch. 7, Ex 4 of Learning Perl
    Make a program that prints each line that has a word that is
    capitalized but not ALL capitalized.
    
    TWO character classes, followed by a plus.
    
    /rs.

---
+++ chapter-07/ex07-04.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+###
+# Chapter 07 Exercise 04
+# Make a program that prints each line that has a word that is capitalized 
+# but not ALL capitalized.
+#
+# usage:
+#   exw07-01.pl
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while ( <DATAFILE> ) {
+		if ( /[A-Z][a-z]+/ ) {
+			print "this line matched: $_";
+		}
+	}
+}
