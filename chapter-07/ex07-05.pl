1f9792a02e607e2699b6c4e413d2311151e32249
Ruddy Stream <ruddystream@gmail.com>
2016-10-13 19:57:24 -0600

    Answer for Ch. 7, Ex 5 of Learning Perl
    Make a program that prints each line that has a two of the same
    nonwhitespace characters next to each other. It should match lines that
    contain words such as Mississippi, Bamm-Bamm, or llama.
    
    I used Unicode Properties to match alpha characters, because those
    weren't around when I started with perl, and I think they're hella neat.
    I also used the /g notation for referring to backreferences. And it
    worked!
    
    /rs.

---
+++ chapter-07/ex07-05.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+###
+# Chapter 07 Exercise 05
+# Make a program that prints each line that has a two of the same 
+# nonwhitespace characters next to each other.
+#
+# usage:
+#   exw07-01.pl
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while ( <DATAFILE> ) {
+		if ( /(\p{alpha})\g{1}/ ) {
+			print "this line matched: $_";
+		}
+	}
+}
