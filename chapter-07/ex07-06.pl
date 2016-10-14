23249a05a91f46cc085e33f6267b4060d70b5809
Ruddy Stream <ruddystream@gmail.com>
2016-10-13 20:28:23 -0600

    Answer for Ch. 7, Ex 6 of Learning Perl
    Write a program that prints out any input line that mentions both wilma
    and fred.
    
    My answer is a little more complex than the book's, as I check for
    Capitalized Names.
    
    I also nearly made a mistake. I originally searched for a pattern of
    Wilma or Fred with characters in between Wilma or Fred. This caught any
    mention of both characters in a line, but it also caught the repeated
    ine, which wasn't correct.
    
    So I changed it to check for Wilma with characters between Fred or Fred
    with characters between Wilma, and that works.
    
    /rs.

---
+++ chapter-07/ex07-06.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+###
+# Chapter 07 Exercise 06
+# Write a program that prints out any input line that mentions both wilma and fred.
+#
+# usage:
+#   exw07-06.pl
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while ( <DATAFILE> ) {
+		if ( /(([fF]red.*[wW]ilma)|([wW]ilma.*[fF]red))/ ) {
+			print "this line matched: $_";
+		}
+	}
+}
