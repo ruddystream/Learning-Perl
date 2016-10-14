24fcde46a84ce0fd94a186293a2219bef7a3ba2d
Ruddy Stream <ruddystream@gmail.com>
2016-10-14 11:58:10 -0600

    Answer for Ch. 7, Ex. 5 of Learning Perl Student Workbook.
    Use a relative backreference to construct a pattern to match a sequence
    of at least three characters that repeats itself at some point in the
    string.
    
    I went down a couple of different paths this time.
    
    First, I matched any charater that repeats itself within the string
    three times. A neat regex, but not correct. Would have lost marks on an
    exam for this!
    
    Second, I used the period to search for any character. This was great,
    but lead to whitespace characters being found. This is technically
    corret, but it didn't feel like it was really right.
    
    Finally, I used the Unicode Property shortcut to match any three Alpha
    characters, and that did what I felt it should do. I like Unicode
    Properties!
    
    /rs.

---
+++ chapter-07/exw07-05.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+###
+# Chapter 07 Workbook Exercise 05
+# Use a relative backreference to construct a pattern to match a sequence 
+# of at least three characters that repeats itself at some point in the 
+# string.
+#
+# usage:
+#   exw07-04.pl 
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while ( <DATAFILE> ) {
+# OOPS: Not a character that repeats itself three times, a three character 
+#       pattern that repeats itself!		
+#		if ( /(\p{alpha}).*\g{-1}.*\g{-1}/ ) {
+# HMMM: This also matches whitespace, which seems not quite right!
+# 		if ( /(...+).*\g{-1}/ ) {
+# WELL: Let's try Unicode Properties!
+		if ( /(\p{Alpha}\p{Alpha}\p{Alpha}+).*\g{-1}/ ) {
+			print "Matched ($1): $_"; 
+		}
+	}
+}
