94b979b56573922cabbdb8d6e165a707a7b765b6
Ruddy Stream <ruddystream@gmail.com>
2016-10-31 19:44:08 -0600

    Answer for Ch. 9, Ex 1 of Learning Perl
    Make a pattern that will match three consecutive copies of whatever is
    currently contained in $what. That is, if $what is fred, your pattern
    should match fredfredfred. If $what is fred|barney, your pattern should
    match fredfredbarney or barneyfredfred or barneybarneybarney or many
    other variations.
    
    I initially used the pattern /($what)($what)($what)/ which, while
    technically corret, was a little inefficient.
    
    /rs.

---
+++ chapter-09/ex09-01.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+###
+# Chapter 09 Exercise 1
+# Make a pattern that will match three consecutive copies of whatever is currently contained in $what.
+#
+# usage:
+#   ex09-01.pl 
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = "./testdata";
+my $what = 'fred|barney';
+
+# Check if the datafile exists.
+if ( ! open DATAFILE, "<", "$datafile" ) {
+	print "Please create and populate $datafile.\n";
+	exit 1;
+} else {
+	while (<DATAFILE>) {							# take one input line at a time
+		chomp;
+	    if (/($what){3}/) {
+    	    print "Matched: |$`<$&>$'|\n";  # the special match vars
+		} else {
+			print "No match: |$_|\n";
+		}
+	}
+}
