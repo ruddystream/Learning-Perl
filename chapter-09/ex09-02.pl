e6bf1efc3118b8cf7238552781b3591f1232bbdf
Ruddy Stream <ruddystream@gmail.com>
2016-10-31 21:07:14 -0600

    Answer for Ch. 9, Ex 2 of Learning Perl
    Write a program that makes a modified copy of a text file. In the copy,
    every string Fred (case-insensitive) should be replaced with Larry. (So,
    Manfred Mann should become ManLarry Mann.) The input file name should be
    given on the command line (don’t ask the user!), and the output filename
    should be the corresponding file name ending with .out.
    
    /rs.

---
+++ chapter-09/ex09-02.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+###
+# Chapter 09 Exercise 2
+# Write a program that makes a modified copy of a text file. In the copy, 
+# every string Fred (case-insensitive) should be replaced with Larry.
+#
+# usage:
+#   ex09-02.pl filename
+#
+# output:
+#   ...
+#
+
+use strict;
+use warnings;
+
+my $datafile = $ARGV[0];
+my $datafileout;
+my $infile;
+my $outfile;
+my $pattern = "fred";
+
+
+if ( ! defined $datafile ) {
+	die "Please supply a filename on the command line.\n";
+}
+
+# Check if the datafile exists.
+if ( ! open $infile, "<", "$datafile" ) {
+	die "Please create and populate $datafile.\n";
+} else {
+	$datafileout = "$datafile.out";
+	if ( ! open $outfile, ">", "$datafileout" ) {
+		die "Couldn't open $datafilecopy for writing, for some reason.\n";
+	}
+	while (<$infile>) {							# take one input line at a time
+		chomp;
+	    s/$pattern/Larry/ig;
+    	print $outfile "$_\n";
+	}
+}
