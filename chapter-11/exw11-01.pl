1373980c0b2af2ddf20fa6eacba94310a66fce08
Ruddy Stream <ruddystream@gmail.com>
2016-11-20 00:24:20 -0600

    Answer for Ch. 11, Ex. 1 of Learning Perl Student Workbook.
    Using the File::Spec module, write a program that takes a directory path
    from the command line and adds it to every file in the current working
    directory to create an absolute path.
    
    The workbook can be a little frustrating, as their answers often use
    language aspects that haven't been covered in the book. I've got
    experience with the language, and am just using these to refresh, but a
    true beginner would likely be lost.
    
    Anyhow...
    
    File::Spec->catfile is the key to this one. That's it.
    
    /rs.

---
+++ chapter-11/exw11-01.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+###
+# Chapter 11 Workbook Exercise 01
+# -------------------------------
+# Using the File::Spec module, write a program that takes a directory path 
+# from the command line and adds it to every file in the current working 
+# directory to create an absolute path.
+#
+# usage:
+#   exw11-01.pl 
+#
+
+use strict;
+use warnings;
+
+use File::Spec;
+use File::Basename qw( basename );
+
+defined $ARGV[0] ? my $path  = $ARGV[0] : die "Need a path.\n";
+my $directory = "/etc/";
+my $DIRHANDLE;
+my @dirlist;
+
+opendir( $DIRHANDLE, $directory) or die "Couldn't open $directory: $!";
+@dirlist = readdir( $DIRHANDLE );
+closedir( $DIRHANDLE );
+
+foreach my $direntry ( @dirlist ) {
+	my $newdirentry = File::Spec->catfile( $path, $direntry );
+	print " $newdirentry\n";
+}
