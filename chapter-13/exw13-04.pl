79b1555a4ca4325433ed9c6a56831edc0c2d424d
Ruddy Stream <ruddystream@gmail.com>
2016-12-07 23:20:50 -0600

    Answer for Ch. 13, Ex. 4 of Learning Perl Workbook.
    Modify your answer to Exercise 13.3 to use glob and a while loop.
    
    Literally just swapped the readdir for a glob.
    
    /rs.

---
+++ chapter-13/exw13-04.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+###
+# Chapter 13 Workbook Exercise 04
+# -------------------------------
+# Modify your answer to Exercise 13.3 to use glob and a while loop.
+#
+
+use strict;
+use warnings;
+use 5.010;
+
+printf " %-25s %10s  %2s %2s %2s\n", "Name", "Size", "R", "W", "X";
+printf "-" x 45 . "\n";
+
+while ( glob "* .*" ) {
+	next if ( -d );
+	my $name = $_;
+	my $size = ( -s ) / 1024;
+	my ( $r, $w, $x ) = ( " ", " ", " " );
+	$r = "x" if ( -r );
+	$w = "x" if ( -w );
+	$x = "x" if ( -x );
+
+	printf " %-25s%10.0f  %2s %2s %2s\n", $name, $size, $r, $w, $x; 
+}
+
+#if ( $count == 1 ) {
+#	$verbage = "is";
+#} else {
+#	$verbage = "are";
+#}
+#
+#print "There $verbage $count files in this directory\n"; 
+#printf "The total size of all the files is %.2f kb.\n", $size;
