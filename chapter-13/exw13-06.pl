4b6fc65d3a5083f7af9315d4a01e381d8eab5d85
Ruddy Stream <ruddystream@gmail.com>
2016-12-07 23:34:35 -0600

    Answer for Ch. 13, Ex. 6 of Learning Perl Workbook.
    Rewrite the previous program to use glob and foreach.
    
    This was a trivial change.
    
    /rs.

---
+++ chapter-13/exw13-06.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+###
+# Chapter 13 Workbook Exercise 06
+# -------------------------------
+# Rewrite the previous program to use glob and foreach.
+#
+
+use strict;
+use warnings;
+use 5.010;
+
+printf " %-25s %10s   %2s %2s %2s\n", "Name", "Size";
+printf "-" x 37 . "\n";
+
+foreach ( glob "* .*" ) {
+	next if ( -d );
+	next if ( ! /\.pl\z/ );
+	my $name = $_;
+	my $size = ( -s ) ;
+	my ( $r, $w, $x ) = ( " ", " ", " " );
+	$r = "x" if ( -r );
+	$w = "x" if ( -w );
+	$x = "x" if ( -x );
+
+	printf " %-25s %9.0fb\n", $name, $size; 
+}
+
+#if ( $count == 1 ) {
+#	$verbage = "is";
+#} else {
+#	$verbage = "are";
+#}
+#
+#print "There $verbage $count files in this directory\n"; 
+#printf "The total size of all the files is %.2f kb.\n", $size;
